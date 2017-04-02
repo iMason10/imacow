c5e67bce587b1286b4cccd677665dbbdaa56da11
Mason <mfryk268@gmail.com>
2017-04-01 23:15:36 -0500

    Update linktoindex.js

--- linktoindex.js
+++ linktoindex.js
@@ -3,7 +3,7 @@ function meow() {
 }
 
 function maketag() {
-  var meow = document.createElement(SPAN);
+  var meow = document.createElement("SPAN");
   var spantxt = document.createTextNode("MEOW");
   meow.appendChild(spantxt);
   document.body.appendChild(meow);
