689cde810426ca4e158d0076ddac4307093170bc
Mason <mfryk268@gmail.com>
2017-04-01 23:21:46 -0500

    Update linktoindex.js

--- linktoindex.js
+++ linktoindex.js
@@ -5,10 +5,12 @@
 console.log("Accessed js sucessfully prot1")
 
 function meow() {
+  console.log("reached meow")
   window.alert("IM A COW");
 }
 
 function meowtxt() {
+  console.log("reached meowtxt")
   var para = document.createElement("P");
   var t = document.createTextNode("This is a paragraph");
   para.appendChild(t);
