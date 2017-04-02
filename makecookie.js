a04fa1d12ac0f29ff8c931bc6b6ee1f8064a0017
Mason <mfryk268@gmail.com>
2017-04-02 16:23:32 -0500

    Update makecookie.js

--- makecookie.js
+++ makecookie.js
@@ -1,1 +1,8 @@
+// Will make cookies if button pressed on an html page
 
+function cowcookies() {
+  console.log("Executed cowcookies")
+  document.cookie = "wow=WOW MUST BUY SHEEP AND COW MILK COMBINED!";
+  document.cookie = "Youtube Designer Layout=eiw9pnZ2Mj8";
+  document.cookie = "Genie=COW GENIE ACTIVATE!";
+  console.log("cowcookies Baked Cookies Sucessfully")
