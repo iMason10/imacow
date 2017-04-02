11112cfaee916c2cf0f907f188410e71bf990879
Mason <mfryk268@gmail.com>
2017-04-01 23:49:02 -0500

    Update alljsforcow.js

--- ext/alljsforcow.js
+++ ext/alljsforcow.js
@@ -1,1 +1,21 @@
+// script for cow.html
 
+console.log("Reached alljsforcow.js")
+console.log("Why am i logging this")
+
+document.cookie = "cow=I LOVE COWS";
+document.cookie = "hatecows=HOW DARE YOU";
+document.cookie = "meytchannel=youtube.com/masonfrykman"
+document.cookie = "blank=Last time I checked its not";
+document.cookie = "hogwarts=Warts on hogs";
+document.cookie = "repo_made=April 1, 2017 11:00-11:45 PM";
+document.cookie = "somany=SO MANY COOKIES MUST EAT ALL;
+
+function deletecookies()
+document.cookie = "cow=I LOVE COWS; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+document.cookie = "hatecows=HOW DARE YOU; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+document.cookie = "meytchannel=youtube.com/masonfrykman; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+document.cookie = "blank=Last time I checked its not; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+document.cookie = "hogwarts=Warts on hogs; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+document.cookie = "repo_made=April 1, 2017 11:00-11:45 PM; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+document.cookie = "somany=SO MANY COOKIES MUST EAT ALL; expires=Thu, 18 Dec 2013 12:00:00 UTC";
