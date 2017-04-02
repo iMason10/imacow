f936b260727acdc6828782e82517bf39303e8b96
Mason <mfryk268@gmail.com>
2017-04-01 23:36:18 -0500

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,1 +1,6 @@
 // Nommey Cookies
+
+document.cookie = "beepbeep=IM A SHEEP";
+document.cookie = "meowmeow=IM A COW";
+document.cookie = "SSID=None Here";
+document.cookie = "my_fav_cookie=all execept raisin";
