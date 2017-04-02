7a616da103b26539f58942574a780a88139524e5
Mason <mfryk268@gmail.com>
2017-04-01 23:13:27 -0500

    Update linktoindex.js

--- linktoindex.js
+++ linktoindex.js
@@ -1,1 +1,10 @@
+function meow() {
+  window.alert("IM A COW");
+}
 
+function maketag() {
+  var meow = document.createElement(SPAN);
+  var spantxt = document.createTextNode("MEOW");
+  meow.appendChild(spantxt);
+  document.body.appendChild(meow);
+}
