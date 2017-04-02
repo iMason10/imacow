8996791fab63c1e86b379407565c978218745a57
Mason <mfryk268@gmail.com>
2017-04-02 00:00:13 -0500

    Update linktoindex.js

--- linktoindex.js
+++ linktoindex.js
@@ -4,6 +4,18 @@
 
 console.log("Accessed js sucessfully prot2");
 console.log("More in the Log for Dev Tests Button");
+console.log("Script version 1.0");
+  console.log("Developer: Mason Frykman");
+  console.log("FOR DEVELOPMENT PURPOSES ONLY");
+  console.log("from ASDFMovie 10 on YouTube");
+  console.log("By TomSka");
+  console.log("If your reading this, you're awesome and dont");
+  console.log("dont yell at me for forgetting to use commas");
+  console.log("and apostraphes");
+  console.log("Thx, Mason (The Creator)");
+  console.log("If you clicked the Log me for dev tests")
+  console.log("That was for me and my friend Jake for dev purposes")
+  window.alert("Go look in the console (if you know what that is) in the browser")
 
 function meow() {
   console.log("reached meow");
