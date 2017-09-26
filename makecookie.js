a1a28c970bb6b5d9e4861efe72251103dff4b149
Mason <mfryk268@gmail.com>
2017-09-25 19:10:59 -0500

    Update makecookie.js

--- makecookie.js
+++ makecookie.js
@@ -5,5 +5,6 @@ function cowcookies() {
   document.cookie = "wow=WOW MUST BUY SHEEP AND COW MILK COMBINED!";
   document.cookie = "Youtube Designer Layout=eiw9pnZ2Mj8";
   document.cookie = "Genie=COW GENIE ACTIVATE!";
+  document.cookie = "omg=urmaghurd";
   console.log("cowcookies Baked Cookies Sucessfully")
 }
